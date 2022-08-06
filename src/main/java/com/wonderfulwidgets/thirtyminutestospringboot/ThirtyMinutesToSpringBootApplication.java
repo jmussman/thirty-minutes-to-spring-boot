cb103b8d71fe91339040f0ed91b0510d855f7560
Joel Mussman <jmussman@smallrock.net>
2022-08-06 17:38:10 -0500

    Initial commit.

---
+++ src/main/java/com/wonderfulwidgets/thirtyminutestospringboot/ThirtyMinutesToSpringBootApplication.java
@@ -0,0 +1,11 @@
+// ThirtyMinutesToSpringBootApplication
+// Copyright © 2022 Joel A Mussman. All rights reserved.
+//
+
+package com.wonderfulwidgets.thirtyminutestospringboot;
+
+public class ThirtyMinutesToSpringBootApplication {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/com/wonderfulwidgets/thirtyminutestospringboot/ThirtyMinutesToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonderfulwidgets.thirtyminutestospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirtyMinutesToSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
